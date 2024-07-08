bab0118f930718e07c50bf8132d0405ed44b1f48
lucasdemouramacedo <lucasdemouramacedo@gmail.com>
2024-07-08 19:59:57 -0300

    feat: update Login page add a responsive design

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -5,20 +5,45 @@ import Button from '../components/Button/Button.jsx';
 
 function Login() {
 
+    const circles = [
+        { width: 150, height: 150, top: '-10%', left: '-5%' },
+        { width: 120, height: 120, top: '-5%', left: '75%' },
+        { width: 150, height: 150, top: '65%', left: '35%' },
+        { width: 80, height: 80, top: '20%', left: '30%' },
+        { width: 120, height: 120, top: '75%', left: '5%' },
+        { width: 110, height: 110, top: '76%', left: '85%' },
+        { width: 50, height: 50, top: '40%', left: '95%' },
+      ];
+
     return (
         <main>
             <div className='logo-container'>
                 <img className='logo' src={Logo} />
             </div>
             <div className='container'>
-                <h1 className='title'>Login</h1>
+                <h2 className='title'>Entre na sua conta</h2>
                 <Input type="text" label="Email"></Input>
                 <Input type="password" label="Senha"></Input>
-                <div className='link'>
-                    <a >Esqueceu sua senha?</a>
+                <div className='forgot-password'>
+                    <a>Esqueceu sua senha?</a>
                 </div>
                 <Button label="Entrar"></Button>
+                <div className='sign-up'>
+                    <span>Ainda não tem uma conta?</span> <a>Cadastre-se</a>
+                </div>
             </div>
+            {circles.map((circle, index) => (
+                <div
+                    className='circle'
+                    key={index}
+                    style={{
+                        width: circle.width,
+                        height: circle.height,
+                        top: circle.top,
+                        left: circle.left,
+                    }}
+                />
+            ))}
         </main>
     )
 }
