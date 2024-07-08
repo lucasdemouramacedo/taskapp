81e1d8f133228ea9f5df2654d9106b56585ebcfb
lucasdemouramacedo <lucasdemouramacedo@gmail.com>
2024-07-08 20:35:49 -0300

    chore: add react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Login from './Login/Login.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 import './index.css'
 import '../global.style.css'
+import Login from './Login/Login.jsx'
+import Register from './Register/Register.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
