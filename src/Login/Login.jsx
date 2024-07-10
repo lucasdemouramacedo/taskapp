530dd3bdf82503767e69cc8fd0d993fc97e989af
lucasdemouramacedo <lucasdemouramacedo@gmail.com>
2024-07-09 22:55:27 -0300

    refactor: change Login page style to module stylesheet

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,4 +1,4 @@
-import './Login.css'
+import style from './Login.module.css'
 import Logo from '.././assets/taskapp.svg';
 import Input from '../components/Input/Input.jsx';
 import Button from '../components/Button/Button.jsx';
@@ -14,28 +14,28 @@ function Login() {
         { width: 120, height: 120, top: '75%', left: '5%' },
         { width: 110, height: 110, top: '76%', left: '85%' },
         { width: 50, height: 50, top: '40%', left: '95%' },
-      ];
+    ];
 
     return (
-        <div className='main'>
-            <div className='logo-container'>
-                <img className='logo' src={Logo} />
+        <div className={style.main}>
+            <div className={style.logoContainer}>
+                <img className={style.logo} src={Logo} />
             </div>
-            <div className='container'>
-                <h2 className='title'>Entre na sua conta</h2>
+            <div className={style.container}>
+                <h2 className={style.title}>Entre na sua conta</h2>
                 <Input type="text" label="Email"></Input>
                 <Input type="password" label="Senha"></Input>
-                <div className='forgot-password'>
+                <div className={style.forgotPassword}>
                     <a>Esqueceu sua senha?</a>
                 </div>
                 <Button label="Entrar"></Button>
-                <div className='sign-up'>
-                    <span>Ainda não tem uma conta?</span> <Link className='link' to="/register">Cadastre-se</Link>
+                <div className={style.signUp}>
+                    <span>Ainda não tem uma conta?</span> <Link className={style.link} to="/register">Cadastre-se</Link>
                 </div>
             </div>
             {circles.map((circle, index) => (
                 <div
-                    className='circle'
+                    className={style.circle}
                     key={index}
                     style={{
                         width: circle.width,
