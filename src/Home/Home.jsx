b8543f5164cf91ef77952d18a7117342f17ee7f3
Lucas Moura <lucasdemouramacedo@gmail.com>
2024-07-25 20:31:04 -0300

    chore: add Home page header

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,23 @@
 import style from './Home.module.css'
+import { MdOutlineCalendarMonth } from "react-icons/md";
+import { CgMenuLeft } from "react-icons/cg";
 
-function Home(){
-    <div className="main">
 
-    </div>
+function Home() {
+    return (
+        <div className={style.main}>
+            <div className={style.header}>
+                <CgMenuLeft className={style.icon} />
+                <MdOutlineCalendarMonth className={style.icon} />
+                <div className={style.info}>
+                    <h2 className={style.date}>Mai, 2024</h2>
+                    <span className={style.qttTasks}>3 tarefas</span>
+                </div>
+            </div>
+            <div className={style.container}>
+            </div>
+        </div>
+    )
 }
 
 export default Home
