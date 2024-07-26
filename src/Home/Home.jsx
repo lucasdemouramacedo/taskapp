2cb0efd9291b860dc61c8c4c9c90f7686dcfac76
Lucas Moura <lucasdemouramacedo@gmail.com>
2024-07-26 20:50:23 -0300

    chore: add Header component to Home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,19 +1,10 @@
 import style from './Home.module.css'
-import { MdOutlineCalendarMonth } from "react-icons/md";
-import { CgMenuLeft } from "react-icons/cg";
-
+import Header from '../components/Header/Header.jsx'
 
 function Home() {
     return (
         <div className={style.main}>
-            <div className={style.header}>
-                <CgMenuLeft className={style.icon} />
-                <MdOutlineCalendarMonth className={style.icon} />
-                <div className={style.info}>
-                    <h2 className={style.date}>Mai, 2024</h2>
-                    <span className={style.qttTasks}>3 tarefas</span>
-                </div>
-            </div>
+            <Header />
             <div className={style.container}>
             </div>
         </div>
