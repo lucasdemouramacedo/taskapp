3233998235aebe0a98524515ad083bbf0ff7a17a
Lucas Moura <lucasdemouramacedo@gmail.com>
2024-07-26 20:49:06 -0300

    chore: add small style to button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,19 @@ import style from './Button.module.css'
 
 function Button(props){
     return(
-        <button className={style.button}>{props.label}</button>
+        <button
+            className={style.button}
+            style={{
+                color: props.color ? props.color : 'var(--white)',
+                backgroundColor: props.backgroundColor ? props.backgroundColor : 'var(--primary-color)',
+                fontSize: props.small ? '11px' : '20px',
+                fontWeight: props.small ? '500' : '600',
+                height: props.small ? '32px' : '50px',
+                borderRadius: props.small ? '10px' : '15px'
+            }}
+        >
+            {props.label}
+        </button>
     );
 }
 
