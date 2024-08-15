78b878ad748022bd04167a6202dbf9ee257efd87
Lucas Moura <lucasdemouramacedo@gmail.com>
2024-08-15 16:59:25 -0300

    refactor: add day to header

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,15 @@
 import style from './Home.module.css'
 import Header from '../components/Header/Header.jsx'
 import DaySelector from '../components/DaySelector/DaySelector.jsx'
+import { useEffect, useState, useRef } from 'react'
+
 function Home() {
+    const [today, setToday] = useState(new Date())
     return (
         <div className={style.main}>
-            <Header />
+            <Header today={today} />
             <div className={style.container}>
-                <DaySelector />
+                <DaySelector today={today} setToday={setToday} />
             </div>
         </div>
     )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,15 @@ import style from './Header.module.css'
 import { MdOutlineCalendarMonth } from "react-icons/md";
 import { CgMenuLeft } from "react-icons/cg";
 import Button from '../Button/Button.jsx'
-import Month from '../../constants/Month.js';
+import Month from '../../constants/MonthEnum.js';
 
-function Header() {
-    const today = new Date()
+function Header(props) {
     return (
         <div className={style.header}>
             <CgMenuLeft className={style.icon} />
             <MdOutlineCalendarMonth className={style.icon} />
             <div className={style.info}>
-                <h2 className={style.date}>{Month.getShort(today.getMonth())}, {today.getFullYear()}</h2>
+                <h2 className={style.date}>{props.today.getDate()} {Month.getShort(props.today.getMonth())}, {props.today.getFullYear()}</h2>
                 <span className={style.qttTasks}>3 tarefas</span>
             </div>
             <div className={style.newTask}>
