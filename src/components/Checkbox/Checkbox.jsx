8096bb3ce3437cf477e81fdaab30747e65f06df3
Lucas Moura <lucasdemouramacedo@gmail.com>
2024-08-19 22:54:10 -0300

    chore: New Checkbox component

---
+++ src/components/Checkbox/Checkbox.jsx
@@ -0,0 +1,26 @@
+import { FaCheck } from "react-icons/fa";
+import style from './Checkbox.module.css'
+import { useState } from 'react'
+
+function Checkbox() {
+    const [state, setState] = useState(false)
+
+    const handleClick = (e) => {
+        setState(!state)
+    };
+    return (
+        <div
+            onClick={handleClick}
+            className={style.checkbox}
+            style={{ backgroundColor: state ? 'var(--primary-color)' : 'var(--grey)' }}
+        >
+            <FaCheck
+                className={style.icon}
+                style={{ color: state ? 'var(--white)' : 'var(--grey)' }}
+            />
+            <input name="check" id="check" type="checkbox" checked={state} />
+        </div>
+    )
+}
+
+export default Checkbox
